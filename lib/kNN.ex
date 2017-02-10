9e738f43dda610c8476136bba55b5882be386549
Michael MacCallum <micksmaccallum@gmail.com>
2017-02-09 20:42:15 -0500

    big bang

---
+++ lib/kNN.ex
@@ -0,0 +1,18 @@
+defmodule KNN do
+  @moduledoc """
+  Documentation for KNN.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KNN.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kNN_test.exs
@@ -0,0 +1,8 @@
+defmodule KNNTest do
+  use ExUnit.Case
+  doctest KNN
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
