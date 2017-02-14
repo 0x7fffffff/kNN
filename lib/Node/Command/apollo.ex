b5117fd3bdf12480e1d446f6f1640e5a113244a3
Michael MacCallum <micksmaccallum@gmail.com>
2017-02-14 01:29:19 +0000

    started working towards tracking node states

--- lib/Node/Command/apollo.ex
+++ lib/Node/Command/apollo.ex
@@ -10,20 +10,53 @@ defmodule KNN.Node.Command.Apollo do
 
 	def reset, do: GenServer.call __MODULE__, :reset
 
-	def init(_state), do: {:ok, %{timer: make_timer()}}
+	def init(_state) do
+    store = %{
+      timer: make_timer(),
+      tracking: %{}
+    }
 
-	def handle_call(:reset, _from, %{timer: timer}) do
+    {:ok, store}
+  end
+
+	def handle_call(:reset, _from, %{timer: timer, tracking: %{}}) do
 		:erlang.cancel_timer timer
-		{:reply, :ok, %{timer: make_timer()}}
+
+    store = %{
+      timer: make_timer(),
+      tracking: %{}
+    }
+
+		{:reply, :ok, store}
 	end
 
-	def handle_info(:tick, _state) do
+  # Need a way of representing planar coordinates that each node represents. Each node should also track node type (store/mesh)
+  # should split store/mesh node counts in roughly half
+  # 
+
+	def handle_info(:tick, %{tracking: %{} = tracking}) do
 		# do stuff here
 		Logger.debug "Tick"
-		Logger.debug inspect Node.list
+    new_tracking = Node.list
+    |> Enum.reduce(%{}, fn(node, acc) -> 
+      acc |> Map.put(node, true)
+    end)
+    |> Map.merge(tracking)
+      
+		Logger.debug inspect new_tracking
+		
+    store = %{
+      timer: make_timer(),
+      tracking: new_tracking
+    }
 
-		{:noreply, %{timer: make_timer()}}
+    {:noreply, store}
 	end
 
-	defp make_timer, do: Process.send_after self(), :tick, 1_000 # every 30 seconds
+  @spec assign_node_type(Node, %{required(Node) => atom}) :: atom
+  defp assign_node_type(node, tracking) do
+    
+  end
+
+	defp make_timer, do: Process.send_after self(), :tick, 5_000 # every 5 seconds
 end
